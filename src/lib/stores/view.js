2def4384a0f42748397502fe05634da8f2cc5855
Dustin Ludwig <info@bit-tag.com>
2025-08-24 08:47:28 +0200

    Dashboard als Standard-Ansicht setzen

--- src/lib/stores/view.js
+++ src/lib/stores/view.js
@@ -1,7 +1,7 @@
 // src/lib/stores/view.js
 import { writable } from 'svelte/store';
 
-export const currentView = writable('desktop'); // 'desktop', 'dashboard' oder 'kanban'
+export const currentView = writable('dashboard'); // 'desktop', 'dashboard' oder 'kanban'
 
 export function switchToDesktop() {
   currentView.set('desktop');
